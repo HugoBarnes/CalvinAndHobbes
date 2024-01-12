2118ef366f4bf44f58f5d9cadee7c452de920611
HugoBarnes <hugo.s.barnes@gmail.com>
2024-01-12 12:21:41 -0500

    all done but for favorites, next, previous, heart

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const pdfPaths = [
+const pdfPaths = {
+    all: [
     'pdfs/(1) Calvin & Hobbes - Bill Watterson.pdf',
     'pdfs/(2) Something Under the Bed is Drooling - Bill Watterson.pdf',
     'pdfs/(3) Yukon Ho! - Bill Watterson.pdf',
@@ -12,10 +13,41 @@ const pdfPaths = [
     'pdfs/(11) Its a Magical World - Bill Watterson.pdf',
     'pdfs/(M) Calvin and Hobbes Miscellaneous - Bill Watterson.pdf',
     'pdfs/(S) Calvin and Hobbes Sunday Pages - Bill Watterson.pdf'
-];
+    ],
+    favoriteslist: [
+    'pdfs/(1) Calvin & Hobbes - Bill Watterson.pdf',
+    'pdfs/(2) Something Under the Bed is Drooling - Bill Watterson.pdf',
+    'pdfs/(3) Yukon Ho! - Bill Watterson.pdf',
+    'pdfs/(4) Weirdos From Another Planet - Bill Watterson.pdf',
+    'pdfs/(5) Revenge of the Baby-Sat - Bill Watterson.pdf',
+    'pdfs/(6) Scientific Progress Goes Boink! - Bill Watterson.pdf',
+    'pdfs/(7) Attack of the Deranged Mutant Killer Monster Snow Goons - Bill Watterson.pdf',
+    'pdfs/(8) The Days are Just Packed - Bill Watterson.pdf',
+    'pdfs/(9) Homocidal Psycho Jungle Cat - Bill Watterson.pdf',
+    'pdfs/(10) Theres Treasure Everywhere - Bill Watterson.pdf',
+    'pdfs/(11) Its a Magical World - Bill Watterson.pdf',
+    'pdfs/(M) Calvin and Hobbes Miscellaneous - Bill Watterson.pdf',
+    'pdfs/(S) Calvin and Hobbes Sunday Pages - Bill Watterson.pdf'
+    ],
+    volume1: ['pdfs/(1) Calvin & Hobbes - Bill Watterson.pdf'],
+    volume2: ['pdfs/(2) Something Under the Bed is Drooling - Bill Watterson.pdf'],
+    volume3: ['pdfs/(3) Yukon Ho! - Bill Watterson.pdf'],
+    volume4: ['pdfs/(4) Weirdos From Another Planet - Bill Watterson.pdf',],
+    volume5: [ 'pdfs/(5) Revenge of the Baby-Sat - Bill Watterson.pdf'],
+    volume6: ['pdfs/(6) Scientific Progress Goes Boink! - Bill Watterson.pdf'],
+    volume7: ['pdfs/(7) Attack of the Deranged Mutant Killer Monster Snow Goons - Bill Watterson.pdf'],
+    volume8: ['pdfs/(8) The Days are Just Packed - Bill Watterson.pdf'],
+    volume9: ['pdfs/(9) Homocidal Psycho Jungle Cat - Bill Watterson.pdf'],
+    volume10: ['pdfs/(10) Theres Treasure Everywhere - Bill Watterson.pdf'],
+    volume11: ['pdfs/(11) Its a Magical World - Bill Watterson.pdf'],
+    volumeM: ['pdfs/(M) Calvin and Hobbes Miscellaneous - Bill Watterson.pdf'],
+    volumeS: ['pdfs/(S) Calvin and Hobbes Sunday Pages - Bill Watterson.pdf'], 
+};
 
 function loadRandomComic() {
-    const randomPdfPath = pdfPaths[Math.floor(Math.random() * pdfPaths.length)];
+    const selectedVolume = document.getElementById('volume-selector').value;
+    selectedPDFPaths = pdfPaths[selectedVolume];
+    const randomPdfPath = selectedPDFPaths[Math.floor(Math.random() * selectedPDFPaths.length)];
 
     pdfjsLib.getDocument(randomPdfPath).promise.then(function(pdfDoc) {
         const numPages = pdfDoc.numPages;
@@ -48,4 +80,13 @@ function loadRandomComic() {
 }
 
 document.getElementById('random-cartoon').addEventListener('click', loadRandomComic);
-loadRandomComic();
+document.getElementById('volume-selector').addEventListener('change', loadRandomComic);
+window.onload = loadRandomComic();
+
+function toggleColor(){
+    const body = document.body;
+    body.classList.toggle('white-background');
+    body.classList.toggle('black-background');
+}
+
+document.getElementById('toggle').addEventListener('click',toggleColor);
